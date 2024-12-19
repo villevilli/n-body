c52916703357d537a73eb15f8031962c523848f4
Villsukka <kujala.ville.vk@gmail.com>
2024-12-19 03:40:29 +0200

    Github Pages Deployment

---
+++ web/wasm_n_body_platformer_bg.wasm.d.ts
@@ -0,0 +1,18 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const main: (a: number, b: number) => number;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_export_1: WebAssembly.Table;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_export_6: WebAssembly.Table;
+export const _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h828e004c50ab5b80: (a: number, b: number, c: number) => void;
+export const closure2810_externref_shim: (a: number, b: number, c: any) => void;
+export const _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h3bfee5e87ee25398: (a: number, b: number) => void;
+export const closure2812_externref_shim: (a: number, b: number, c: any, d: any) => void;
+export const _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__he79b0a3959efadf4: (a: number, b: number) => void;
+export const closure103953_externref_shim: (a: number, b: number, c: any) => void;
+export const __wbindgen_start: () => void;
