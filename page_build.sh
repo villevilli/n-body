d89605b9de5d02037eecbe5e3ce8f44cf48f84c3
Villsukka <kujala.ville.vk@gmail.com>
2024-12-22 20:23:36 +0200

    update page build for multiple binaries

--- page_build.sh
+++ page_build.sh
@@ -3,6 +3,6 @@ echo "Building Rust"
 cargo build --target wasm32-unknown-unknown --profile wasm-release
 
 echo "Generating Wasm Bindings"
-wasm-bindgen --out-name wasm_n_body_platformer \
+wasm-bindgen --out-name sim_test \
 --out-dir web/ \
---target web target/wasm32-unknown-unknown/wasm-release/n-body-platformer.wasm
+--target web target/wasm32-unknown-unknown/wasm-release/sim_test.wasm
