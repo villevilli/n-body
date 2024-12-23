28dc98416a5aebb6a6e7b3143da680e4bfb557ad
Villsukka <kujala.ville.vk@gmail.com>
2024-12-23 02:39:31 +0200

    adds egui as dependancy and an egui window

--- src/bin/cmdline_test.rs
+++ src/bin/cmdline_test.rs
@@ -1,12 +1,19 @@
 use bevy::prelude::*;
+use bevy_egui::{egui, EguiContexts, EguiPlugin};
 use n_body_platformer::commands::DevCommandline;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugins(EguiPlugin)
         .add_plugins(DevCommandline)
         .add_systems(Startup, setup)
+        .add_systems(Update, ui_system)
         .run();
+}
+
+fn ui_system(mut context: EguiContexts) {
+    egui::Window::new("Hello Gui").show(context.ctx_mut(), |ui| ui.label("World"));
 }
 
 fn setup(mut commands: Commands) {
